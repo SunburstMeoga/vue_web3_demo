a2d9fbcb3b562024c19a9a3d53c6dafca775ca10
apple <apple@appledeMacBook-Air.local>
2022-12-06 18:40:14 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Web3 from 'web3'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      web3: 'this is web3'
+    }
+  },
+  mounted() {
+    this.initWeb3()
+  },
+  methods: {
+    initWeb3() {
+      //本地私有网络地址
+      let host = "http://127.0.0.1:8545"
+      //创建providers
+      const httpprovi = new Web3.providers.HttpProvider(host)
+      this.web3 = new Web3(httpprovi)
+      console.log(this.web3)
+    }
   }
 }
 </script>
